17f0aff6853b6480ff82346c56f5ded04cf78bdb
jason <rodriguezjasonlloyd@gmail.com>
2025-12-12 18:18:06 +0800

    feat: move imports inside block and exit the process after update

--- src/updater.rs
+++ src/updater.rs
@@ -1,13 +1,9 @@
-#[cfg(target_os = "windows")]
-use std::io;
-
 use color_eyre::eyre::Result;
 
 #[cfg(target_os = "windows")]
-use self_update::{backends::github::Update, cargo_crate_version};
-
-#[cfg(target_os = "windows")]
 pub fn check_and_update() -> Result<()> {
+    use self_update::{backends::github::Update, cargo_crate_version};
+
     let status = Update::configure()
         .repo_owner("rodriguezjasonlloyd")
         .repo_name("ovarai")
@@ -17,11 +13,14 @@ pub fn check_and_update() -> Result<()> {
         .update()?;
 
     if status.updated() {
+        use std::{io, process};
+
         println!(
             "Updated to version {}, press any key to continue...",
             status.version()
         );
         io::stdin().read_line(&mut String::new())?;
+        process::exit(0);
     }
 
     Ok(())
